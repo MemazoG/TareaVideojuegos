389f2f64a9aa84ef009dbfa682a7060ea09795a0
MemazoG <a01283254@itesm.mx>
2020-03-31 17:37:15 -0600

    Movimiento
    Movimiento en ejes x, y y z.
    Para z es con las teclas 'Z' y 'X'

---
+++ Videojuegos Hello World/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * speed;
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.position += new Vector3(0, 0, 1) * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            transform.position += new Vector3(0, 0, -1) * Time.deltaTime * speed;
+        }
+    }
+}
